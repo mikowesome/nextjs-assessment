9ca7f4c446e3748d948685ddd606177577ebf705
Miko <jm_mapilisan@yahoo.com>
2022-12-01 11:49:43 +0800

    add more nav links

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,14 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const toggleNavbar = () => setShowNavbar(prev => !prev)
+
   return (
     <nav className="bg-white border-b border-gray-200 fixed w-full dark:bg-gray-900 z-10">
-        <div className="flex items-center justify-between px-4 py-2.5 md:px-6 max-w-3xl mx-auto sm:px-6 lg:max-w-5xl lg:px-0">
+        <div className="flex flex-wrap items-center justify-between px-4 py-2.5 md:px-6 max-w-3xl mx-auto sm:px-6 lg:max-w-5xl lg:px-0">
             <Link href='/' className='flex items-center'>
                 <img 
                     src='./logo-no-background.png' 
@@ -14,6 +19,33 @@ const Navbar = () => {
                     Cybersecurity
                 </span>
             </Link>
+            <button 
+                type="button" 
+                className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                onClick={toggleNavbar}
+            >
+                <span className="sr-only">Open main menu</span>
+                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            </button>
+            <div className={`${showNavbar ? '' : 'hidden'} w-full md:block md:w-auto`}>
+                <ul className="flex flex-col p-4 mt-4 border md:mr-10 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                    </li>
+                    <li>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                    </li>
+                    <li>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                    </li>
+                    <li>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
   )
