dd79f66af1a49e93277a7c777a3a4d088be3c67a
Miko <jm_mapilisan@yahoo.com>
2022-12-01 10:54:18 +0800

    add z index to overlap animated elements

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="bg-white border-b border-gray-200 fixed w-full dark:bg-gray-900">
+    <nav className="bg-white border-b border-gray-200 fixed w-full dark:bg-gray-900 z-10">
         <div className="flex items-center justify-between px-4 py-2.5 md:px-6 max-w-3xl mx-auto sm:px-6 lg:max-w-5xl lg:px-0">
             <Link href='/' className='flex items-center'>
                 <img 
--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,21 @@ const Home = ({ articles }) => {
             <link rel="icon" href="/logo-no-background.png" />
         </Head>
         <main className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-5xl lg:px-0">
-            <header className="flex items-center justify-center min-h-screen hero">
-                <h1 className="text-3xl text-center font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl xl:text-6xl">
-                    Cybersecurity threats 101: What a cybersecurity expert understands: a blog about different cyber threats and how you can protect yourself.
+            <header className="flex flex-col items-center justify-center min-h-screen hero">
+                <h1 className="text-3xl text-center font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl xl:text-6xl mb-4">
+                    Cybersecurity threats 101
                 </h1>
+                <p className="mb-6 text-lg text-center font-medium text-gray-500 lg:text-3xl sm:text-xl xl:text-5xl dark:text-gray-400">
+                    What a cybersecurity expert understands: A blog about different cyber threats and how you can protect yourself.
+                </p>
+                <a className="animate-bounce cursor-pointer" href="#articles">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </a>
             </header>
-            {/* <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" /> */}
-            <div className="mb-20 lg:grid lg:grid-cols-2">
+            {/* <hr className="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" /> */}
+            <div id="articles" className="mb-20 lg:grid lg:grid-cols-2 scroll-mt-6">
                 {
                     articles.map(article => <Article key={article.id} article={article} />)
                 }
