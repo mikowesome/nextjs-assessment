8f0699a9535da96ab2226e1d2c49ce1079c721fd
Miko <jm_mapilisan@yahoo.com>
2022-12-01 10:44:21 +0800

    add scroll smooth on html element

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html className="scroll-smooth">
+        <Head />
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+  )
+}
+export default Document
